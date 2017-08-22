4a980594b3af2234c2ae73e53f82eca1e2c3cfc7
s179061 <jacobletko@outlook.com>
2017-08-21 17:25:18 -0700

    finshed chapter 7

--- 7-arrays/main.cpp
+++ 7-arrays/main.cpp
@@ -9,23 +9,23 @@ using namespace std;
 
 int main()
 {
-	int numbers[]{1,3,7,6,9};
+	int numbers[5];
 	srand(time(NULL));
 
-	//for (int i = 0; i < 5; i++)
-		//numbers[i] = rand() % 10 + 1;
+	for (int i = 0; i < 5; i++)
+		numbers[i] = rand() % 10 + 1;
 	
 	int size = (sizeof(numbers) / sizeof(*numbers));  //math for size of array if unknown
 	
 	printNumbers(numbers, size);
-	/*sumNumbers(numbers, size);
+	sumNumbers(numbers, size);
 	LargestValue(numbers, size);
 
 	findIndex(numbers, size, 4);
 	uniqueness(numbers, size, numbers, size);
 	reverse(numbers, size);
 	ascendSort(numbers, size);
-	descendSort(numbers, size);*/
+	descendSort(numbers, size);
 
 	cout << endl;
 
